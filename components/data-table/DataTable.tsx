373d804de6ccd7e0d5a5a1ba74c9e93080caa118
m <139261114+mauriciorfdev@users.noreply.github.com>
2026-07-18 23:09:36 -0400

    feat(products): update states, loading logic in DataTable
    - rename dataModal to ingredientModal and localData to productsData
    - add isLoading state and render TableLoader during data fetching

--- components/data-table/DataTable.tsx
+++ components/data-table/DataTable.tsx
@@ -6,40 +6,58 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import type { IngredientFilters } from '../../src/types.ts';
 import styles from './DataTable.module.css';
+import TableLoader from '../table-loader/TableLoader.tsx';
+
+const TableHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Brand</th>
+        <th>Ingredients</th>
+        <th>List</th>
+      </tr>
+    </thead>
+  );
+};
 
 interface DataTableProps {
   filters: IngredientFilters;
 }
-/* ({filters}:{filters:IngredientFilters}) */
 
 const DataTable = ({ filters }: DataTableProps) => {
   const [show, setShow] = useState(false);
-  const [dataModal, setDataModal] = useState('');
+  const [ingredientModal, setIngredientModal] = useState('');
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const [localData, setLocalData] = useState<any[]>([]);
+  const [productsData, setProductsData] = useState<any[]>([]);
   const noResultsMessage = `We couldn't find any products with that ingredient.`;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   //Fetch all products data
   useEffect(() => {
-    fetchData();
+    fetchProducts();
   }, []);
 
-  async function fetchData() {
+  async function fetchProducts() {
     const url = `${API_URL}/products`;
     try {
+      setIsLoading(true);
       const resp = await fetch(url);
-      const localData = await resp.json();
-      setLocalData(localData);
-      console.log(localData);
+      const productsData = await resp.json();
+      setProductsData(productsData);
+      console.log(productsData);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   //Filter products by ingredient (input)
 
-  const filteredData = localData.filter((product) => {
+  const filteredData = productsData.filter((product) => {
     //true if product includes ingredient (input)
     const includeIngredient = product.ingredients
       .toLowerCase()
@@ -82,22 +100,15 @@ const DataTable = ({ filters }: DataTableProps) => {
   return (
     <div className={styles.container}>
       <h1>Products ({filteredData.length})</h1>
+      <p>{!isLoading && filteredData.length == 0 && noResultsMessage}</p>
 
-      {filteredData.length == 0 ? (
-        noResultsMessage
-      ) : (
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Brand</th>
-              <th>Ingredients</th>
-              <th>List</th>
-            </tr>
-          </thead>
+      <Table striped hover>
+        <TableHead />
+
+        {isLoading ? (
+          <TableLoader cols={5} message='Loading products...' />
+        ) : (
           <tbody>
-            {/*  */}
             {filteredData.map((product, index) => {
               const ingredients = product.ingredients;
               const ingredientsArr = getIngredientsList(ingredients);
@@ -119,7 +130,7 @@ const DataTable = ({ filters }: DataTableProps) => {
                           size='sm'
                           onClick={() => {
                             handleShow();
-                            setDataModal(ingredients);
+                            setIngredientModal(ingredients);
                           }}
                         >
                           Ver más
@@ -133,8 +144,8 @@ const DataTable = ({ filters }: DataTableProps) => {
               );
             })}
           </tbody>
-        </Table>
-      )}
+        )}
+      </Table>
 
       <Modal
         show={show}
@@ -145,12 +156,11 @@ const DataTable = ({ filters }: DataTableProps) => {
         <Modal.Header closeButton>
           <Modal.Title>Ingredients List</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{dataModal}</Modal.Body>
+        <Modal.Body>{ingredientModal}</Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          {/* <Button variant='primary'>Ok</Button> */}
         </Modal.Footer>
       </Modal>
     </div>
